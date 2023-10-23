dd2013c3cfa09a49a180e08505639bd426488059
Twicez <hectorgrajales479@gmail.com>
2023-10-22 19:50:36 -0700

    Se agrego Administrador Controllers/Routes, falta pruebas (Pendientes)

---
+++ routes/AdministradorRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const AdministradorController = require('../controllers/AdministradorController');
+
+
+router.post('/crear', AdministradorController.crearAdministrador);
+router.get('/', AdministradorController.obtenerAdministradores);
+router.get('/:id', AdministradorController.obtenerAdministradorPorId);
+router.put('/:id', AdministradorController.actualizarAdministrador);
+router.delete('/:id', AdministradorController.eliminarAdministrador);
+
+module.exports = router;
