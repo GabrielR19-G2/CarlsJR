5fe0b76afe8f45794194bf5ed7ec9b0cce3dc0e7
Alan Gonzalez <alanfga1@gmail.com>
2023-10-28 02:33:52 -0700

    correccion de errores en daos y endpoints

--- routes/usuarioRouter.js
+++ routes/usuarioRouter.js
@@ -2,15 +2,9 @@ const express = require("express");
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
 
-<<<<<<< HEAD
 // Obtener usuarios
 router.get('/', usuariosController.getUsuarios);
 router.get('/:id', usuariosController.getUsuarioPorId);
-=======
-//obtener usuarios
-router.get('/', usuariosController.getUsuarios)
-router.get('/:id', usuariosController.getUsuarioPorId)
->>>>>>> 65285be97f6d0e50fcb61ae84455b899444b9fa1
 
 // Agregar usuarios
 router.post('/', usuariosController.addUsuario);
